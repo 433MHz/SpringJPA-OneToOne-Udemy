d730b4f98c4a709800b4513f8bd76674500a24dd
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-11 20:40:02 +0200

    Initialized

---
+++ src/main/java/pl/krystian/SpringJpaOneToOneUdemyApplication.java
@@ -0,0 +1,13 @@
+package pl.krystian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOneToOneUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaOneToOneUdemyApplication.class, args);
+	}
+
+}
